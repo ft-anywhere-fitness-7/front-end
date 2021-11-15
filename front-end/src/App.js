6e0bc140d10623c7e11fafc07b9116b502b49f85
Tuan147 <90153951+Tuan147@users.noreply.github.com>
2021-11-15 17:24:08 -0500

    added in the footer section with basic styles

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,28 @@
 import Header from "./components/Header";
-
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div className="content-wrap">
+     <div className="header-section"><Header /></div>
       <h1>Fitness</h1>
+      <h1>Fitness</h1>
+      <h1>Fitness</h1>
+      <h1>Fitness</h1>
+      <h1>Fitness</h1>
+      <h1>Fitness</h1>
+      <h1>Fitness</h1>
+      <h1>Fitness</h1>
+      <h1>Fitness</h1>
+      <h1>Fitness</h1>
+      </div>
+      <div className="footer-page">
+      <Footer/>
+      </div>
     </div>
+    
   );
 }
 
---
+++ front-end/src/components/Footer.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Footer.css";
+
+
+const Footer = () => {
+
+    return (
+        <div className="footer-section">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>Anywhere Fitness</h4>
+                        <ul className="list-unstyled">
+                            <li>Sign In</li>
+                            <li>Register</li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <h4>Meet the team!</h4>
+                        <ul className="list-unstyled">
+                            <li>Brandi Ball</li>
+                            <li>Cam Meiners</li>
+                            <li>Collyn Godlewski</li>
+                            <li>James Calvo</li>
+                            <li>Kat Yevsukov</li>
+                            <li>Rongjun Geng</li>
+                            <li>Tuan Nguyen</li>
+                        </ul>
+                    </div>
+                    <hr />
+                    <div className="row">
+                        <p className="col-sm">
+                            &copy;{new Date().getFullYear()} Anywhere Fitness | All rights reserved | Terms of Service | Privacy
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
