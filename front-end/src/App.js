e216a5d86d4591e91645e57e85b1e5c5f6376de9
James_Calvo <15jcalvo@gmail.com>
2021-11-15 16:48:36 -0500

    pulled header update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import Home from './Home';
 import Header from "./components/Header";
 
 
@@ -7,12 +5,8 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Home />
-=======
       <Header />
       <h1>Fitness</h1>
->>>>>>> main
     </div>
   );
 }
