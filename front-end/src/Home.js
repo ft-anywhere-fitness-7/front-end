874c207483b2d1c49c806ae2c91ea91ae5010fd4
James_Calvo <15jcalvo@gmail.com>
2021-11-15 16:45:37 -0500

    Homepage no style

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import homeImg from './assets/fitness3.jpg'
+
 export default function Home(props) {
 
     return(
         <div>
             <div className='container'>
-            <div className='left container'>
-                <div>
-                    <h1>Welcome to Anywhere Fitness</h1>
+                <div className='left container'>
+                    <div>
+                        <h1>Welcome to Anywhere Fitness</h1>
+                    </div>
+                    <div>
+                        <p>THESE DAYS, FITNESS CLASSES CAN BE HELD ANYWHERE- A PARK, AN UNFINISHED BASEMENT OR A GARAGE- NOT JUST AT A TRADITIONAL GYM. CERTIFIED FITNESS INSTRUCTORS NEED AN EASY WAY TO TAKE THE AWKWARDNESS OUT OF ATTENDANCE TAKING AND CLIENT PAYMENT PROCESSING.</p>
+                    </div>
+                    <div>
+                        <button>Register Now</button>
+                    </div>
                 </div>
-                <div>
-                    <p>THESE DAYS, FITNESS CLASSES CAN BE HELD ANYWHERE- A PARK, AN UNFINISHED BASEMENT OR A GARAGE- NOT JUST AT A TRADITIONAL GYM. CERTIFIED FITNESS INSTRUCTORS NEED AN EASY WAY TO TAKE THE AWKWARDNESS OUT OF ATTENDANCE TAKING AND CLIENT PAYMENT PROCESSING.</p>
+                <div className='right container'>
+                    <img src={homeImg} alt="A woman with a barbell on her back"/>
                 </div>
-                <div>
-                    <button>Register Now</button>
-                </div>
-            </div>
-            <div className='right container'>
-                <img src={homeImg} alt="A woman with a barbell on her back"/>
-            </div>
             </div>
         </div>
     )
