9b5d55ba6ce6d922f0f6f6274103ac7f70ada0f9
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-16 12:58:12 -0800

    add states for App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,15 @@ import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
+
+  // To carry the whole classes 
+  const [classes, setClasses] = useState([]);
+
+  // The state for header
+  const isLoggedIn = localStorage.getItem('token');
+  const role = localStorage.getItem('role');
+  const username = localStorage.getItem('username');
+
   return (
     <div className="App">
       <div className="content-wrap">
