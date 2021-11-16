8e67d7e3b4e2a337ee5be0a20c89278b8174725b
James_Calvo <15jcalvo@gmail.com>
2021-11-16 13:43:13 -0500

    react-router-dom installed

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,7 @@
 import Header from "./components/Header";
-
 import Footer from "./components/Footer";
-
 import Home from "./components/Home";
-
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
