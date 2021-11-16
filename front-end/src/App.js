7ed005df245a784ad6d46918fd14351af076507c
James_Calvo <15jcalvo@gmail.com>
2021-11-16 14:47:43 -0500

    Rendering register

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Classes from "./components/Classes";
+import Register from "./components/Register";
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
             <Routes>
               <Route exact path='/' element={<Home/>}/>
               <Route exact path='/login' element={<div></div>}/>
-              <Route exact path='/register' element={<div></div>}/>
+              <Route exact path='/register' element={<Register/>}/>
               <Route exact path='/classes' element={<Classes/>}/>
             </Routes>
         </div>
