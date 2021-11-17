29624f5411c2496c8d801397358a1c6448d3206b
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-17 06:57:52 -0800

    fix the folder CSS location

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import homeImg from '../assets/fitness3.jpg'
-import './CSS/Home.css';
+import '../CSS/Home.css';
 import { Link } from 'react-router-dom'
 
 export default function Home(props) {
--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,5 +1,4 @@
 import React from "react";
-import './CSS/Login.css'
 
 export default function Login (props){
   
