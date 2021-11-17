09f51573b7cf105dd785f24a158f57fbd77ff094
James_Calvo <15jcalvo@gmail.com>
2021-11-16 23:16:38 -0500

    class cards build need to format strings

--- front-end/src/components/Class.js
+++ front-end/src/components/Class.js
@@ -7,9 +7,18 @@ export default function Class (props) {
             <div>
                 <h1>{props.i.class_name}</h1>
             </div>
+            <div className="intensity">
+                <h5>
+                    {props.i.intensity_level}
+                </h5>
+            </div>
             <div>
-                <span className="date">Date: {props.i.class_date}</span>
-                <span className="time">Length: {props.i.class_duration}</span>
+                <span className="date">Date: {props.i.class_date} Time: {props.i.start_time}</span>
+                <span className="length">Length: {props.i.class_duration}</span>
+            </div>
+            <div>
+                <span className="instructor">Date: {props.i.instructor}</span>
+                <span className="location">Location: {props.i.class_location}</span>
             </div>
         </div>
     )
