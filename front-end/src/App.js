82f545a9956b8d0ccb6013a03bb6b070ddce0d5d
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-11-20 01:55:20 -0800

    update isLoggedin logic

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,6 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import ClassList from "./components/ClassList";
 import Class from "./components/Class"
-import ClassSignup from "./components/ClassSignup";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Logout from "./components/Logout";
@@ -16,9 +15,7 @@ function App() {
   // To carry the whole classes 
   const [classes, setClasses] = useState([]);
   const [users, setUsers] = useState([]);
-  const [user_id, setUser_id] = useState(null);
-  const [class_id, setClass_id] = useState(null);
-
+  
 
   const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('token'));
 
@@ -35,56 +32,24 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <div className="content-wrap">
-        <div className="header-section">
-          
-        </div>
-        <div className='display'>
-            <Switch>
-              <Route exact path="/"> 
-                <Home  />           
-              </Route>        
-              <Route path="/login">
-                <Login setIsLoggedIn = { setIsLoggedIn } isLoggedIn = { isLoggedIn } role = {role} message = {message} setUser_id = {setUser_id} />
-              </Route> 
-              <Route path="/logout">
-                <Logout setIsLoggedIn = { setIsLoggedIn } />
-              </Route> 
-              <Route path="/register">
-                <Register />
-              </Route> 
+    <div className="App">     
+        
+        <Switch>
+              <Route exact path="/"> <Home  /> </Route>        
+              <Route path="/login"> <Login setIsLoggedIn = { setIsLoggedIn } isLoggedIn = { isLoggedIn } role = {role} message = {message} /> </Route> 
+              <Route path="/logout"> <Logout setIsLoggedIn = { setIsLoggedIn } /> </Route> 
+              <Route path="/register"> <Register /> </Route> 
+              <Route path="/user" render={props => <User {...props} deleteMovie={deleteUser} />} />
+              <Route path="/users"> <UserList users = { users } setUsers = { setUsers } /> </Route> 
 
-              <Route
-              path="/user"
-              render={props => <User {...props} deleteMovie={deleteUser} />}              
-              />
-
-              <Route path="/users">
-              <UserList users = { users } setUsers = { setUsers } />
-              </Route> 
-
-              <Route path="/classes/:id">
-              <Class classes = { classes } deleteClass={deleteClass} message = {message} isLoggedIn = {isLoggedIn} role = {role} setClass_id = {setClass_id} />  </Route>
-
-              <Route path="/signupclass">
-                <ClassSignup users ={users} class = {classes} />
-              </Route> 
-
-              <Route path="/classes">
-                <ClassList classes = { classes } setClasses = { setClasses} message = {message} isLoggedIn = {isLoggedIn} role = {role} />
-              </Route> 
-
-              {/* <Route path='/create'>
-                <CreateClass />
-              </Route> */}              
-
-            </Switch>
-        </div>
+              <Route path="/classes/:id"> <Class classes = { classes } deleteClass={deleteClass} message = {message} isLoggedIn = {isLoggedIn} role = {role}  /> </Route>
+              <Route path="/classes"> <ClassList classes = { classes } setClasses = { setClasses} message = {message} isLoggedIn = {isLoggedIn} role = {role} /> </Route> 
+        </Switch>
+        
         <div className="footer-page">
           <Footer/>
         </div>
-      </div>
+     
     </div>
     
   );
--- front-end/src/components/Class.js
+++ front-end/src/components/Class.js
@@ -5,13 +5,10 @@ import axiosWithAuth from '../utils/axiosWithAuth'
 const Class = (props) => {      
     
     const [session, setSession] = useState('');
-
     const { class_id, class_name, intensity_level,class_date, start_time, class_duration, instructor, class_location} = session;
 
     const { id } = useParams();
-    const { push } = useHistory();
-
-    console.log('id: ', id);
+    const { push } = useHistory();    
 
     useEffect(() => {
             axiosWithAuth()
@@ -32,15 +29,15 @@ const Class = (props) => {
     return(<>
     
     <nav className="nav-bar">
-                    <div className="left-links">
-                        <Link className="link" to='/'>Anywhere Fitness &nbsp; {props.message}  </Link>                                                  
-                    </div>
-                    <div className="right-links">                                                         
-                        {(props.role === 'instructor' && props.isLoggedIn) && <Link className="link"  to='/create'>Create a Class</Link>  }                 
-                        {(props.role === 'client' && props.isLoggedIn) && <Link className="link"  to='/create'>Jion a Class</Link> }             
-                        {props.isLoggedIn && <Link className="link" to='/logout'>Logout</Link>}                        
-                    </div>
-                    </nav>
+        <div className="left-links">
+            <Link className="link" to='/'>Anywhere Fitness &nbsp; {props.message}  </Link>                                                  
+        </div>
+        <div className="right-links">                                                         
+            {(props.role === 'instructor' && props.isLoggedIn) && <Link className="link"  to='/create'>Create a Class</Link>  }                 
+            {(props.role === 'client' && props.isLoggedIn) && <Link className="link"  to='/create'>Jion a Class</Link> }             
+            {props.isLoggedIn && <Link className="link" to='/logout'>Logout</Link>}                        
+        </div>
+    </nav>
 
     <div className="modal-page col">
         <div className="modal-dialog">
@@ -64,12 +61,9 @@ const Class = (props) => {
                             <div> <label>Class id: <strong>{session.class_id}</strong></label> </div>                                                    
                         </section>
                         <section>
-                            <div>
-                                <span className="m-2 btn btn-dark">Add Class</span>
-                                    
-                                    <Link to={`/classes/${class_id}`} className="m-2 btn btn-success">Edit</Link>
-                                <span className="delete" onClick = {handelDelete}  ><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
-                            </div>
+                            {props.isLoggedIn && <span className="m-2 btn btn-dark">Add Class</span> }                                   
+                            {props.isLoggedIn && <Link to={`/classes/${class_id}`} className="m-2 btn btn-success">Edit</Link>}
+                            {props.isLoggedIn && <span className="delete" onClick = {handelDelete}  ><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>}                            
                         </section>       
 
                     </div>
